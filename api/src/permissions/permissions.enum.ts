68ecaf202370d0f0e77acfd21851a3ca99fe40f0
luan.prestes <luan.prestes@mysmartclinic.com.br>
2024-11-15 15:03:52 -0300

    Feat: creating base to permissions

---
+++ api/src/permissions/permissions.enum.ts
@@ -0,0 +1,5 @@
+export enum Role {
+  ADMIN = 1,
+  EDITOR = 2,
+  READER = 3,
+}
--- api/src/users/users.service.ts
+++ api/src/users/users.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { User } from '@prisma/client';
+import { Role } from 'src/permissions/permissions.enum';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  async createUser(
+    name: string,
+    email: string,
+    password: string,
+    permissionId: Role,
+  ): Promise<User> {
+    const user = await this.prisma.user.create({
+      data: {
+        name,
+        email,
+        password,
+        permissionId,
+      },
+    });
+
+    return user;
+  }
+}
